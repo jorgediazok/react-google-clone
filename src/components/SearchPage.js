efd3ee0b91735d97c3f5dab771dd00c226b5aa9e
Jorge <jorgediazok@gmail.com>
2020-10-19 19:02:09 -0300

    created the google search hook and used it

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useStateValue } from '../context/StateProvider';
 import '../styles/SearchPage.css';
+import useGoogleSearch from './useGoogleSearch';
 
 function SearchPage() {
+  const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term);
+  console.log(data);
+
   return (
-    <div>
-      <h1>Hello from SearchPage</h1>
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+      </div>
+      <div className="searchPage__results"></div>
     </div>
   );
 }

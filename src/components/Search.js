1f965e04d05b7be1b84d7158ff66a3a0af045ab7
Jorge <jorgediazok@gmail.com>
2020-10-19 15:17:31 -0300

    catching the values from the input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 import '../styles/Search.css';
 
 function Search() {
+  const [input, setInput] = useState('');
+  const history = useHistory();
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log('You hit search', input);
+    history.push('/search');
+  };
+
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon />
       </div>
 
       <div className="search__buttons">
-        <Button variant="outlined">Google search</Button>
+        <Button type="submit" onClick={search} variant="outlined">
+          Google search
+        </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   );
 }
 

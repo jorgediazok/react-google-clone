6eaeabfe3f29d921b3b0974babffb4bc5f4ea50f
Jorge <jorgediazok@gmail.com>
2020-10-19 02:34:43 -0300

    created all components and accomodate the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Home from './pages/Home';
+import SearchPage from './pages/SearchPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1> First Commit 🚀 </h1>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/SearchPage.css';
+
+function SearchPage() {
+  return (
+    <div>
+      <h1>Hello from SearchPage</h1>
+    </div>
+  );
+}
+
+export default SearchPage;
